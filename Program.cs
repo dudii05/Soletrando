732d823ddefb86601c9827817370346ec1c68705
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-12 20:37:12 -0300

    Resolução da atividade

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿﻿Console.WriteLine("Soletrar");
+
+Console.Write("escreva aqui seu texto: ");
+string soletrar;
+soletrar=Console.ReadLine()!;
+for (int i = 0; i < soletrar.Length; i++)
+{
+    Console.Write(soletrar[i]);
+    if (i < soletrar.Length-1)
+    {
+        Console.Write("-");
+    }
+}
+Console.WriteLine();
